e49d37128f7c1c8f727bb6737d43eb5604faccb5
Sadiya8924 <sadiyamaritza8924@gmail.com>
2024-02-22 09:36:48 +0700

    menambahkan 2 parameter

--- jobsheet2/BukuMain24.java
+++ jobsheet2/BukuMain24.java
@@ -12,5 +12,10 @@ public class BukuMain24{
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        Buku24 bk2 = new Buku24("Self Reward", "Mahera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+        
     }
 }
